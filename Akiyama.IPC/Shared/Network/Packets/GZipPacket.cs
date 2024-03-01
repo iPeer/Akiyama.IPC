d73443fa4694450409bd501acce9cbd5c0f8b250
iPeer <chrisdwignall+github@gmail.com>
2024-03-01 04:11:59 +0000

    QoL: Docstring disclaimer about size changes for small payloads on `GZipPacket`

--- Akiyama.IPC/Shared/Network/Packets/GZipPacket.cs
+++ Akiyama.IPC/Shared/Network/Packets/GZipPacket.cs
@@ -3,6 +3,13 @@ using System.IO.Compression;
 
 namespace Akiyama.IPC.Shared.Network.Packets
 {
+    /// <summary>
+    /// A packet used for transferring compressed data.
+    /// <br /><br />
+    /// <b>Note</b>: Packets with small amounts of data may result in diminishing or even negative returns when using this packet type.
+    /// <br />If the size of the data being transferred by this packet is small, it may be better to use a <see cref="GenericDataPacket"/> instead.
+    /// <br />The performance of your application(s) may also be impacted due to the overhead from (de)compressing data.
+    /// </summary>
     public class GZipPacket : Packet
     {
         public override int ID => (int)PacketType.GZIP;
